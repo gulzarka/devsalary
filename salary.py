2b3b59735ddb59141c31a873bad7a2d43cfbb7cf
gulzarka <80506357+gulzarka@users.noreply.github.com>
2023-01-15 00:18:55 +0600

    edited variable names

--- salary.py
+++ salary.py
@@ -3,18 +3,24 @@ import requests
 from terminaltables import AsciiTable
 from dotenv import load_dotenv
 
-HH_URL = 'https://api.hh.ru/vacancies/'
-SJ_URL = 'https://api.superjob.ru/2.0/vacancies/'
-LANGUAGES = ['Python','Java Script', 'Ruby', 'Go', 'C', 'C++', 'CSS', 'PHP', 'C#', 'Java']
+LANGS = ['Python', 'Java Script', 'Ruby', 'Go', 'C', 'C++', 'CSS', 'PHP', 'C#', 'Java']
 
-def get_info_all_pages_for_HH(language):
+
+
+def get_response_all_pages_HH(language):
+    url = 'https://api.hh.ru/vacancies/'
+    moscow_code = 1
+    publishing_days = 1
     page = 0
     pages_number = 1
     pages_response = []
     while page < pages_number:
-        params = {'page': page, 'text': f'Программист {language}', 
-        'area': '1','only_with_salary': True,  'period': '1'}         
-        page_response = requests.get(HH_URL, params)
+        params = {
+            'page': page, 'text': f'Программист {language}',
+            'area': moscow_code, 'only_with_salary': True, 
+            'period': publishing_days
+        }  
+        page_response = requests.get(url, params)
         page_response.raise_for_status()
         page_payload = page_response.json()
         pages_number = page_payload['pages']
@@ -23,22 +29,22 @@ def get_info_all_pages_for_HH(language):
     return pages_response
        
 
-def get_salary_info_for_HH(response):
+def get_salaries_HH(response):
     salaries_from = []
     salaries_to = []
     for vacancies in response:
-        vacancy = vacancies['items']
         vacancies_found = vacancies['found']
-        for vacancy in vacancy:
+        vacancies = vacancies['items']
+        for vacancy in vacancies:
             salary_currency = vacancy['salary']['currency']
             if salary_currency == 'RUR':
                 salary_from = vacancy['salary']['from']
                 salaries_from.append(salary_from)
                 salary_to = vacancy['salary']['to']
                 salaries_to.append(salary_to)
-            None        
-    return {'salary_from': salaries_from, 
-            'salary_to': salaries_to,
+            None       
+    return {'salaries_from': salaries_from,
+            'salaries_to': salaries_to,
             'found': vacancies_found}
 
 
@@ -46,39 +52,45 @@ def calculate_average_salary(salary_from, salary_to):
     if salary_from and not salary_to:
         return round(salary_from * 1.2)
     if salary_to and not salary_from:
-        return round(salary_to * 0.8)    
-    if salary_from  and salary_to:
+        return round(salary_to * 0.8) 
+    if salary_from and salary_to:
         return (salary_from + salary_to) // 2  
-    return False   
-        
- 
-def get_vacancy_statistics(salary_info, language):        
-    vacancies_found = salary_info['found']
+    return False
+
+
+def get_vacancy_statistics(salaries, language):
     average_salaries = []
-    for salary_from in salary_info['salary_from']:
+    vacancies_found = salaries['found']
+    salaries_from = salaries['salaries_from']
+    for salary_from in salaries_from:
         salary_from = salary_from
-    for salary_to in salary_info['salary_to']:
+    for salary_to in salaries['salaries_to']:
         salary_to = salary_to
         average_salary = calculate_average_salary(salary_from, salary_to)
         average_salaries.append(average_salary)
     total_average_salary = sum(average_salaries) // len(average_salaries)
-    output = {language: {'vacancies_found': vacancies_found,  
+    vacancy_statistics = {language: {'vacancies_found': vacancies_found, 
                         'vacancies_processed': len(average_salaries),
                         'average_salary': total_average_salary}}  
-    return output 
-   
-                 
-def get_info_all_pages_for_SJ(language):
+    return vacancy_statistics
+
+           
+def get_response_all_pages_SJ(language):
     load_dotenv('access_token.env')
     sj_access_token = os.environ['SUPER_JOB_TOKEN']
+    url = 'https://api.superjob.ru/2.0/vacancies/'
+    moscow_code = 4
     page = 0
     pages_number = 1
     pages_response = []
+    only_with_salary = 1
+    programmer_id = 48
     while page < pages_number:
-        params = {'town': 4, 'keyword': language, 'no_agreement': 1, 
-                'page': page, 'currency': 'rub', 'catalogues': 48}
+        params = {'town': moscow_code, 'keyword': language, 
+                    'no_agreement': only_with_salary,
+                'page': page, 'currency': 'rub', 'catalogues': programmer_id}
         headers = {'X-Api-App-Id': sj_access_token}
-        page_response = requests.get(SJ_URL, headers=headers, params=params)
+        page_response = requests.get(url, headers=headers, params=params)
         page_response.raise_for_status()
         page_payload = page_response.json()
         vacancy_count = page_payload['total']
@@ -89,19 +101,19 @@ def get_info_all_pages_for_SJ(language):
     return pages_response
 
 
-def get_salary_info_for_SJ(response):
+def get_salaries_SJ(response):
     salaries_from = []
-    salaries_to =[]
+    salaries_to = []
     for vacancies in response:
-        vacancy = vacancies['objects']
+        vacancies = vacancies['objects']
         vacancies_found = vacancies['total']
-        for vacancy in vacancy:  
+        for vacancy in vacancies:  
             salary_from = vacancy['payment_from']
             salaries_from.append(salary_from)
             salary_to = vacancy['payment_to'] 
             salaries_to.append(salary_to)
-    return {'salary_from': salaries_from,
-            'salary_to': salaries_to,
+    return {'salaries_from': salaries_from,
+            'salaries_to': salaries_to,
             'found': vacancies_found} 
 
 
@@ -110,9 +122,9 @@ def get_stat_table_HH():
                     'Vacancies found',
                     'Vacancies processed',
                     'Average salary')]                               
-    for language in LANGUAGES: 
-        vacancy_info = get_salary_info_for_HH(get_info_all_pages_for_HH(language))
-        vacancy_statistics = get_vacancy_statistics(vacancy_info, language)        
+    for language in LANGS: 
+        vacancies = get_salaries_HH(get_response_all_pages_HH(language))
+        vacancy_statistics = get_vacancy_statistics(vacancies, language)        
         for language, salary_stat in vacancy_statistics.items():
             lang = language
             found = salary_stat['vacancies_found']
@@ -130,8 +142,8 @@ def get_stat_table_SJ():
                     'Vacancies processed',
                     'Average salary'
                 )]        
-    for language in LANGUAGES: 
-        vacancy_info = get_salary_info_for_SJ(get_info_all_pages_for_SJ(language))
+    for language in LANGS: 
+        vacancy_info = get_salaries_SJ(get_response_all_pages_SJ(language))
         vacancy_statistics = get_vacancy_statistics(vacancy_info, language)        
         for language, salary_stat in vacancy_statistics.items():
             lang = language
@@ -141,6 +153,7 @@ def get_stat_table_SJ():
             vacancies_table.append((lang, found, processed, salary))  
     table_instance = AsciiTable(vacancies_table, 'Super Job Moscow')
     return table_instance.table     
+
 
 def main():
     print(get_stat_table_HH())
